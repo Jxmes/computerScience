e3b045963a8125acc9fdefed70437e2f6cb7dec1
Alfie Bowman <bowmanalfie@gmail.com>
2015-09-16 11:56:26 +0100

    Update bronzeCert.py
    fixed some syntax and style errors

--- bronzeCert.py
+++ bronzeCert.py
@@ -3,11 +3,10 @@
 # name: Alfie Bowman
 # description: Some basic Python tasks
 
-print ("Welcome to Python")
+print("Welcome to Python")
 
-print (10 + 5)
+print(10 + 5)
 
-""" Created by Alfie 'A1fus' Bowman on 09/09/2015 """
-""" Comments might be useful to help explain your code to other people who might use it """
-
-print ("Hello, how are you? \nI hope you are well today.")
+#Created by Alfie 'A1fus' Bowman on 09/09/15
+#Comments might be useful to help explain your code to other people who might use it
+print("Hello, how are you? \nI hope you are well today.")
