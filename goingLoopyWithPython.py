34317172bc8b0cf6ec512181e7fac30bc4804cea
Alfie Bowman <bowmanalfie@gmail.com>
2015-09-16 12:49:06 +0100

    Create goingLoopyWithPython.py

---
+++ goingLoopyWithPython.py
@@ -0,0 +1,21 @@
+# date: 11/09/15
+# username: A1fus
+# name: Alfie Bowman
+# description: Going Loopy with Python
+
+lines = 0    #defines variable
+while lines <50:    #causes Python to do anything indented until the condition is met
+    print("I will not mess about in Computer Science lessons")    #prints the str
+    lines = lines + 1    #adds 1 to the variable
+
+
+value = 1    #Aspire
+while value < 101:
+    print(value)
+    value = value + 1
+
+
+yourNumber = int(input("Pick a number: "))    #Aspire+
+while yourNumber < 101:
+    print(yourNumber)
+    yourNumber = yourNumber + 1
