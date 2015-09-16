53f95678e072e623665f8829c38f773e518e7a82
Alfie Bowman <bowmanalfie@gmail.com>
2015-09-16 12:37:21 +0100

    Create silverCert.py

---
+++ silverCert.py
@@ -0,0 +1,14 @@
+numberOne = 72
+print (numberOne)
+
+#A variable is something that can store another value
+number1 = 10
+number2 = 5
+print (number1 + number2)
+
+number3 = int(input("your first number: "))
+number4 = int(input("your first number: "))
+print (number3 + number4)
+
+name = input ("what is your name? ")
+print ("Welcome to Python, ", name)
