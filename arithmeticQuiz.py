d73940719299370d0a047e02d283f2ac5967cfd5
Alfie Bowman <bowmanalfie@gmail.com>
2015-09-18 11:26:15 +0100

    Update arithmeticQuiz.py

--- arithmeticQuiz.py
+++ arithmeticQuiz.py
@@ -1,12 +1,22 @@
 import random
 import time
 
-counter = 0
+counter = 1
 score = 0
 numberOne = 0
 numberTwo = 0
 operator = 0
 
 while counter < 10:
+  numberOne = random.randint(0,11)
+  numberTwo = random.randint(0,11)
+  operator = random.randint(0,4)
   
+  if operator == 1:
+    operator = +
+  elif operator == 2:
+    operator = -
+  else:
+    operator = *
   
+  answer = int(input("Question ",counter,numberOne,operator,numberTwo))
