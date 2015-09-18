e3c315e6285b7699c926e152bb88ae75dbe471ee
Alfie Bowman <bowmanalfie@gmail.com>
2015-09-18 11:50:10 +0100

    Update arithmeticQuiz.py

--- arithmeticQuiz.py
+++ arithmeticQuiz.py
@@ -11,12 +11,13 @@ for counter in range(0,10):
   numberOne = random.randint(0,11)
   numberTwo = random.randint(0,11)
   operator = random.choice(operatorList)
+  print(numberOne, operator, numberTwo)
   
-  question = "numberOne operator numberTwo"
-  answer = eval(question)
-  print (question, "=???")
-  reply int(input("Enter your answer: ")
-  if reply == answer:
-    print "Correct!"
-  else
-    print ("Unlucky!")
+  #question = numberOne, operator, numberTwo
+  #answer = eval(question)
+  #print (question, "=???")
+  #reply = int(input("Enter your answer: "))
+  #if reply == answer :]
+  #  print ("Correct!")
+  #else :
+  #  print ("Unlucky!")
