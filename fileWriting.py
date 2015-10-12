a434f4dce4a99c62883a2f7d6f109372a7d566ff
Alfie Bowman <bowmanalfie@gmail.com>
2015-10-12 12:42:11 +0100

    Create fileWriting.py

---
+++ fileWriting.py
@@ -0,0 +1,9 @@
+file = open("text.txt","w")
+
+print("Alex makes really good pancakes and tuna sandwiches")
+
+file.write("hello\n")
+file.write("Alex makes really good pancakes and tuna sandwiches")
+file.close()
+
+print("text written")
