dddf6d58bb992d922db203fab966d9ae8bdbb8ff
James <imslloths@gmail.com>
2015-10-14 11:29:50 +0100

    Imported thew newer version of James' Quiz

--- jamesQuiz.py
+++ jamesQuiz.py
@@ -1,60 +1,64 @@
-import random
-import time
-import sys
+#Name: James Hancock
+#Candidate No. 6078
+#Outwood Academy - 23148
+#A453 Programming Task
+
+import random #imports the default Python random module, which enables the user to generate random integers
+import time #imports the default Python time module, which enables the user to add pauses in the program
 
 #variables
-name = input("What's your name? ")
-counter = 0
-answer = ""
-userscore = 0
-begin = input("Are you ready? ")
+name = input("What's your name? ") #asks the user for their name
+counter = 0 #counts up so the program knows how many questions have been asked
+answer = "" #asks for the answer so that the program can check if the answer is correct or incorrect
+userscore = 0 #score counter so that the user knows how many questions they got right out of 10
+begin = input("Are you ready? ") #asks if the user is ready to start the quiz
 
-time.sleep(1)
+time.sleep(1) #creates a delay for 1 second
 
-if begin == "yes":
+if begin == "yes": #if the user replies "yes" to the question, run the indented code
 
-    time.sleep(1)
+    time.sleep(1) #creates a delay for 1 second
 
-    print("Welcome to the Maths Quiz",name,"!")
+    print("Welcome to the Maths Quiz",name,"!") #prints "Welcome to the Maths Quiz (name)?"
 
-    time.sleep(1)
+    time.sleep(1) #creates a delay for 1 second
 
-    while counter<10:
-     number1 = random.randint(0,20)
-     number2 = random.randint(0,15)
-     operators = random.randint(1,3)
-
-     if operators == 1:
+    while counter<10: #while creates a loop
+     number1 = random.randint(0,20) #randomly generates an integer between 0 and 20
+     number2 = random.randint(0,15) #randomly generates an integer between 0 and 15
+     operators = random.randint(1,3) #randomly generates one of the following operators, *,+, or -
+    
+     if operators == 1: #when the operator is equal to 1, run the indented code
         print("What is",number1,"+",number2,)
-        ans = number1 + number2
-        counter = counter+1
+        ans = number1 + number2 #addition
+        counter = counter+1 #adds 1 to the question counter
 
-     elif operators == 2:
+     elif operators == 2: #when the operator is equal to 2, run the indented code
         print("What is",number1,"*",number2,)
-        ans = number1 * number2
-        counter = counter+1
+        ans = number1 * number2 #multiplication
+        counter = counter+1 #adds 1 to the question counter
 
-     else:
+     else: #when the operator isn't 2 or 3, run the indented code
         print("What is",number1,"-",number2,)
-        ans = number1 - number2
-        counter = counter+1
+        ans = number1 - number2 #subtraction
+        counter = counter+1 #adds 1 to the question counter
 
-     useranswer = int(input())
-     if useranswer == ans:
-        print("Correct!")
-        userscore = userscore + 1
+     useranswer = int (input()) 
+     if useranswer == ans: #if the answer is correct, run the indented code
+        print("Correct!") #prints "Correct!" if the answer was correct
+        userscore = userscore + 1 #adds 1 to the user's score
      else:
-        print("Wrong!")
+        print("Wrong!") #prints "Wrong!" if the answer was incorrect
 
-    if userscore<5:
-        time.sleep(1)
-        print("Better luck next time",name,"you scored",userscore,"/ 10!")
+    if userscore<5: #if the user's score is less than 5, run the indented code
+        time.sleep(1) #creates a delay for 1 second
+        print("Better luck next time",name,"you scored",userscore,"/ 10!") #prints "Better luck next time"(name)"you scored",(score), / 10!" if the score was less than 5
     else:
-        time.sleep(1)
-        print("Congratulations",name,"you scored",userscore,"/ 10!")
+        time.sleep(1) #creates a delay for 1 second
+        print("Congratulations",name,"you scored",userscore,"/ 10!") #prints "Congratulations"(name)"you scored",(score), / 10!" if the score was more than 5
 
 else:
-    print("James isn't worth oxygen!")
-    sys.exit()
+    print("Okay nevermind!") #prints "Okay nevermind!" if anything else other than "yes" is entered after, "Are you ready?"
 
-    time.sleep(4)
+time.sleep(4) #creates a 4 second delay before the program exits
+
